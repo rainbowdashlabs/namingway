22087e19168027121325cd49d887252ffe9d6628
Lilly <46890129+RainbowDashLabs@users.noreply.github.com>
2024-04-01 09:45:28 +0200

    Bring back lost names more frequent

--- src/main/java/de/chojo/namingway/services/MentionListener.java
+++ src/main/java/de/chojo/namingway/services/MentionListener.java
@@ -59,7 +59,7 @@ public class MentionListener extends ListenerAdapter {
     }
 
     private void diceNames(Member first, Member second) {
-        if (current().nextDouble() <= 0.1) {
+        if (current().nextDouble() <= 0.2) {
             // Bring back a lost name
             switchNames(config.secondary(Users.KEY).randomName(), current().nextDouble() >= 0.5 ? first : second);
             return;
