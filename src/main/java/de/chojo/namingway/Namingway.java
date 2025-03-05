f27f62ac44042fab4850c8f4e22f49217f156c55
Lilly <46890129+RainbowDashLabs@users.noreply.github.com>
2025-03-05 14:31:49 +0100

    Register rename command

--- src/main/java/de/chojo/namingway/Namingway.java
+++ src/main/java/de/chojo/namingway/Namingway.java
@@ -1,6 +1,7 @@
 package de.chojo.namingway;
 
 import de.chojo.jdautil.interactions.dispatching.InteractionHub;
+import de.chojo.namingway.commands.Rename;
 import de.chojo.namingway.commands.Rollback;
 import de.chojo.namingway.commands.Release;
 import de.chojo.namingway.configuration.ConfigFile;
@@ -36,8 +37,10 @@ public class Namingway {
 
         InteractionHub.builder(shardManager)
                 .testMode()
-                .withCommands(new Rollback(config),
-                        new Release(config))
+                .withCommands(
+                        new Rollback(config),
+                        new Release(config),
+                        new Rename(config))
                 .build();
     }
 }
